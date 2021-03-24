6fb4e6c35062c1ab5b9984a934d26910976734c7
Maksym <77988267+MaksymYefimenko@users.noreply.github.com>
2021-03-24 13:52:22 +0100

    Tests Calculator

--- src/test/java/firstTest/beforeAndAfterMetodsTest.java
+++ src/test/java/firstTest/beforeAndAfterMetodsTest.java
@@ -6,7 +6,7 @@ import org.testng.annotations.*;
 public class beforeAndAfterMetodsTest {
     @BeforeClass
     public void setUp() {
-        System.out.println(" Before class" );
+        System.out.println(" Before class");
     }
 
     @AfterClass
@@ -15,38 +15,39 @@ public class beforeAndAfterMetodsTest {
     }
 
     @Test
-    public void testIntPositiveNumbers () {
-       int actual = 1 + 2;
-       int expected = 3;
+    public void testIntPositiveNumbers() {
+        int actual = 1 + 2;
+        int expected = 3;
         Assert.assertEquals(actual, expected);
     }
+
     @Test
     public void testIntNegativeNumbers() {
         int actual = -1 - -2;
         int expected = -1;
         Assert.assertEquals(actual, expected);
     }
+
     @Test
-    public void testDoubleNumbers () {
+    public void testDoubleNumbers() {
         double actual = 1.5 + 2.99;
         double expected = 4.44;
     }
+
     @BeforeMethod
     public void beforeTests() {
-      System.out.println("Before method");
+        System.out.println("Before method");
     }
+
     @AfterMethod
     public void afterTest() {
         System.out.println("After method");
     }
+
     @DataProvider(name = "testDataForAddition")
-    public Object [][] testDataForAddition() {
+    public Object[][] testDataForAddition() {
         return new Object[][]{
                 {1, 2, 3}
         }
-
-        @Test(dataProvider = "testDataForAddition")
-        public void additionTeatWitDataProvider () {
-
-        }
     }
+}
